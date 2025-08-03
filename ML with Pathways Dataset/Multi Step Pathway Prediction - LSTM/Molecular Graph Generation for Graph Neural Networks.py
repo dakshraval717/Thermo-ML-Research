431cf1b304e40a986faab1e4d798cb6a76425505
Daksh Raval <dakshraval717@gmail.com>
2025-08-03 13:19:08 -0600

    got adjaceny matrix, atom and bond characteristic parsing working

--- ML with Pathways Dataset/Multi Step Pathway Prediction - LSTM/Molecular Graph Generation for Graph Neural Networks.py
+++ ML with Pathways Dataset/Multi Step Pathway Prediction - LSTM/Molecular Graph Generation for Graph Neural Networks.py
@@ -9,6 +9,7 @@ from rdkit.Chem import rdmolops # for graph generation and operations/manipulati
 smiles = 'CCO'  # Example SMILES string for ethanol
 molecule = Chem.MolFromSmiles(smiles)
 
+# nodes of graph neural network are atoms in the molecule
 atoms = []
 for atom in molecule.GetAtoms():
     atoms.append({
@@ -19,9 +20,29 @@ for atom in molecule.GetAtoms():
         'Formal Charge': atom.GetFormalCharge(), # Lewis structure formal charge
         'Hybridization': atom.GetHybridization().name, # valence bond hybridization state (sp, sp2, tetrahedral sp3, planar sp3, etc.)
         'Aromatic?': atom.GetIsAromatic(), # whether the atom is part of an aromatic ring
-        ''
-        ''
-        ': 
+        'In Benzene Ring?': atom.GetIsAromatic(), # whether the atom is part of a benzene ring
+        'In Ring?': atom.IsInRing(), # whether the atom is part of any ring
     }
     )
+
+# edges of graph neural network are bonds in the molecule
+bonds = []
+for bond in molecule.GetBonds():
+    bonds.append({
+        '1st Atom Index': bond.GetBeginAtomIdx(),
+        '2nd Atom Index': bond.GetEndAtomIdx(),
+        'Bond Type': bond.GetBondType().name, # type of bond (single, double, triple, aromatic)
+        'Aromatic?': bond.GetIsAromatic(), 
+        'Conjugated?': bond.GetIsConjugated(), # part of chain of double/triple-single-double/triple bonds
+        'In Ring?': bond.IsInRing()
+    })
+
+
+# specifies which nodes (atoms) are connected; chemical with N atoms has N x N adjacency matrix
+adj_matrix = rdmolops.GetAdjacencyMatrix(molecule)
+print("Adjacency Matrix:")
+print(adj_matrix)
+
+print(atoms)
+print(bonds)
 
